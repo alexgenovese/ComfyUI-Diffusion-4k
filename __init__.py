57f9305683e78887482a6d8ced3108639e26f520
Alex <423745+alexgenovese@users.noreply.github.com>
2025-03-29 10:40:05 +0100

    install requirements on load if not found diffusers and missed class

--- __init__.py
+++ __init__.py
@@ -1,5 +1,20 @@
-from .diffusion_4k import FluxImageGenerator
+import os, sys
 
+# Installazione automatica delle dipendenze
+def install_dependencies():
+    try:
+        import diffusers
+    except ImportError:
+        print("Installazione delle dipendenze per Diffusion 4k...")
+        import subprocess
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+        print("Dipendenze installate con successo!")
+
+# Esegui l'installazione delle dipendenze quando il modulo viene caricato
+install_dependencies()
+
+
+from .diffusion_4k import FluxImageGenerator
 
 # Mappatura dei nodi per ComfyUI
 NODE_CLASS_MAPPINGS = {
